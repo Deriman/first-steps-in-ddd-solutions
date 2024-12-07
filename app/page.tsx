66ac0dd0dbfbf5e465d98a654fe8ae0b96cd8e28
Jose C. Santos <zderimanz@hotmail.com>
2024-12-07 19:19:41 +0100

    ff

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
       <main className="flex flex-col items-center p-24">
-        <span>Hola mundo!!!!</span>
+        <span>Hola mundo!!!!bb</span>
       </main>
   );
 }
